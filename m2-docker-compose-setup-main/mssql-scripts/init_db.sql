5b7a2aa3be11b1a785a2736321d228a3ee227aed
Sanja Salihovic <sanjasalihovic@Sanjas-MacBook-Pro.local>
2022-10-25 14:47:13 +0200

    Created user table

--- m2-docker-compose-setup-main/mssql-scripts/init_db.sql
+++ m2-docker-compose-setup-main/mssql-scripts/init_db.sql
@@ -1,7 +1,16 @@
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
+ CREATE TABLE [TEST_DATABASE].[dbo].[User]  
+(  
+    id INT NOT NULL UNIQUE,
+    FullName VARCHAR(256) NOT NULL,
+    Email VARCHAR(256) NOT NULL,
+    Password NVARCHAR(32),
+    Role VARCHAR NOT NULL  
 );
+
+
+ALTER TABLE [TEST_DATABASE].[dbo].[User] ALTER COLUMN userRole VARCHAR(256) NOT NULL
+
+EXEC sp_rename 'User.userFullName', 'fullName'
+EXEC sp_rename 'User.userEmail', 'email'
+EXEC sp_rename 'User.userPassword', 'password'
+EXEC sp_rename 'User.userRole', 'role'
