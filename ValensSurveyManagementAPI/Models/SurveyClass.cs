2b5692848c8dc4026eefe35f5dd6c15d952b692d
Sanja Salihovic <sanjasalihovic@192.168.1.9>
2022-10-27 01:31:33 +0200

    created class for surveys

---
+++ ValensSurveyManagementAPI/Models/SurveyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ValensSurveyManagementAPI.Models
+{
+    public class SurveyClass
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string startAt { get; set; }
+        public DateTime endAt { get; set; }
+        public string description { get; set; }
+        public User createdBy { get; set; }
+
+    }
+}
+
